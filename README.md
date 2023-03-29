e61632f1e4c766bbe6541027698977decbc66380
Joshua Trimm <jbthype@gmail.com>
2023-03-28 22:34:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MatsNav
+ 
---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("navBar").innerHTML = `<nav class="navBar">
+<ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="About.html">About</a></li>
+    <li><a href="Contact.html">Contact</a></li>
+    <li><a href="Menu.html">Menu</a></li>
+</ul>
+</nav>`;
